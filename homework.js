d2c8b5f2d79418c5e14f34f555141ea8c9331ebf
itsdeebarnett <jdeebarnett@gmail.com>
2021-05-19 08:59:13 -0400

    homework

--- homework.js
+++ homework.js
@@ -5,18 +5,64 @@
 //
 // If the potential member is not in the array, return the array unchanged.
 // If the potential member is in the array, remove all instances of it from the array.
+function remove (people, member) {
+    let newArray = []
+        
+    for (let i = 0; i < people.length; i++) {
+        if (people[i] !== member) {
+        newArray.push(people[i])}
 
+    }
+    return newArray
+}
 // 2. Revisit your "remove" function. Make sure that it does not change the original
 // array but instead returns a new array.
+function remove (people, member) {
+    let newArray = []
+        
+    for (let i = 0; i < people.length; i++) {
+        if (people[i] !== member) {
+        newArray.push(people[i])}
+
+    }
+    return newArray
+}
 
 // 3. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
+function sum (number) {
+    let box = 0
+    for (let x of number){
+        box += x
+    }
+return box
+}
 
 // 4. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
+function average (number) {
+    let count = number.length
+    let newSum = sum(number)
+    if ( number.length === 0) {
+        return undefined
+    }else  {
+                return newSum / count
+            }
+        }
 
 // 5. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
+function minimum (number) {
+    let small = 0
+    for (let x of number) {
+        if (x === 0){
+            return undefined
+        } else if (x < small || small === 0){
+            small = Math.min(...number)
+            return small
+        }
+    }
+}
 
 // 6. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
@@ -41,6 +87,9 @@
 // Note 2: Selection sort can be implemented using one array. Read the explanation at
 // https://courses.cs.vt.edu/csonline/Algorithms/Lessons/SelectionSort/index.html
 // to see how. This may make more sense to you.
+
+
+
 
 // 7. Create a function called `textList` that takes an array and joins its elements
 // into a string separated by commas.
